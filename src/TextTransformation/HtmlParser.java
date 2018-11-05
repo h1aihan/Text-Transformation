08ed3f665567337c48e486d786980c324a6fd0df
Adam <adamthk@gmail.com>
2018-11-05 17:49:33 -0500

    Define all major classes from UML, add some TODOs

---
+++ src/TextTransformation/HtmlParser.java
@@ -0,0 +1,9 @@
+package TextTransformation;
+
+
+//TODO: Document class definition
+public class HtmlParser {
+	// TODO: Implement
+	
+	
+}
---
+++ src/TextTransformation/OutputDataStructure.java
@@ -0,0 +1,23 @@
+package TextTransformation;
+import java.util.HashMap;
+
+
+// TODO: Document class definition
+public class OutputDataStructure {
+	// TODO: Document member variable
+	// TODO: Create public accessors
+	private HashMap<String, Integer> wordGram;
+	// TODO: Document member variable
+	// Gram type
+	// 	1 - Word
+	// 	2 - BiGram
+	// 	3 - TriGram
+	//  ...
+	// TODO: Create public accessors
+	private int gramN;
+	
+	private HashMap<String, Integer> metaData;
+	
+	
+
+}
