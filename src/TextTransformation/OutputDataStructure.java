c598b96153a281c6ca9b75aea10fcfb59fe72f32
Adam <adamthk@gmail.com>
2018-11-09 16:48:17 -0500

    Makes NgramMap and OutputDataStructure Compatible

--- src/TextTransformation/OutputDataStructure.java
+++ src/TextTransformation/OutputDataStructure.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package TextTransformation;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -11,7 +10,7 @@ import org.json.*;
 public class OutputDataStructure {
 	// TODO: Document member variable
 	// TODO: Create public accessors
-	private HashMap<String, NGram> wordGrams;
+	private HashMap<String, NgramMap> wordGrams;
 	// TODO: Document member variable
 	// Gram type
 	// 	1 - Word
@@ -34,7 +33,7 @@ public class OutputDataStructure {
 	public JSONObject getNGramJSON() throws JSONException {
 		JSONObject ngramJSON = new JSONObject();
 		for (String key : wordGrams.keySet()) {
-			ngramJSON.put(key, wordGrams.get(key).getMap());
+			ngramJSON.put(key, wordGrams.get(key));
 		}
 		return ngramJSON;
 	}
@@ -66,70 +65,4 @@ public class OutputDataStructure {
 	}
 
 }
-=======
-package TextTransformation;
-import java.util.HashMap;
-import java.util.HashSet;
-import org.json.*;
 
-
-// TODO: Document class definition
-public class OutputDataStructure {
-	// TODO: Document member variable
-	// TODO: Create public accessors
-	// TODO: Document member variable
-	// Gram type
-	// 	1 - Word
-	// 	2 - BiGram
-	// 	3 - TriGram
-	//  ...
-	// TODO: Create public accessors
-	private HashSet<String> links;
-	private HashMap<String, String> metaData;
-	private HashMap<String, NgramMap> ngramsFromTags;
-	
-	public JSONObject metaDataJSON() throws JSONException  {
-		JSONObject metaJSON = new JSONObject();
-		metaJSON.put("meta", metaData);
-		return metaJSON;
-	}
-	
-	public JSONObject linksJSON() throws JSONException {		
-		JSONObject metaJSON = new JSONObject();
-		metaJSON.put("links", links);
-		return metaJSON;
-	}
-	
-	public JSONObject ngramJSON() throws JSONException {
-		JSONObject metaJSON = new JSONObject();
-		metaJSON.put("ngrams", ngramsFromTags);
-		return metaJSON;
-	}
-	public String metaDataToString() throws JSONException {
-		return metaDataJSON().toString();
-	}
-	
-	public String linksToString() throws JSONException {
-		return linksJSON().toString();
-	}
-	public String ngramToString() throws JSONException {
-		return ngramJSON().toString();
-	}
-
-	public String toString() {
-		JSONObject concatenated = new JSONObject();
-		try {
-			JSONObject metaJSON = metaDataJSON();
-			concatenated.put("meta", metaJSON.get("meta"));
-			JSONObject linksJSON = linksJSON();
-			concatenated.put("links", linksJSON.get("links"));
-			JSONObject ngramJSON = ngramJSON();		
-			concatenated.put("ngram", ngramJSON.get("ngram"));
-		} catch (Exception e) {
-			return concatenated.toString();
-		}
-		return concatenated.toString();
-	}
-
-}
->>>>>>> 58e8b9e9539cb0dcb816b6ae949f7199d2112052
