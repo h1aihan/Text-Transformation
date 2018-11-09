cefcab411a42cf8176fb51546380b595b792bc49
haih2@rpi.edu <haih2@rpi.edu>
2018-11-09 16:43:18 -0500

    Add parserUrl function

--- src/TextTransformation/HtmlParser.java
+++ src/TextTransformation/HtmlParser.java
@@ -1,13 +1,27 @@
 package TextTransformation;
 
+import java.util.List;
 import org.json.*;
 
+import com.linkedin.urls.Url;
+import com.linkedin.urls.detection.UrlDetector;
+import com.linkedin.urls.detection.UrlDetectorOptions;
 
 // TODO: Consider making static
 //TODO: Document class definition
 public class HtmlParser {
 	// TODO: Implement
-	
+	public List<Url> parserUrl(String html){
+		 UrlDetector parser = new UrlDetector(html, UrlDetectorOptions.Default);
+		    List<Url> found = parser.detect();
+
+		    for(Url url : found) {
+		        System.out.println("Scheme: " + url.getScheme());
+		        System.out.println("Host: " + url.getHost());
+		        System.out.println("Path: " + url.getPath());
+		    }
+		    return found; 
+	}
 	
 	public OutputDataStructure parse(JSONObject json) throws Exception {
 		// TODO: Implement
