e7cebd195cc4c336b07c12f62d00663869621b1a
haih2@rpi.edu <haih2@rpi.edu>
2018-11-09 17:29:28 -0500

    add unit test htmlparser

---
+++ src/TestTextTransformation/UnitTestHtmlParser.java
@@ -0,0 +1,18 @@
+package TestTextTransformation;
+import TextTransformation.*;
+import org.junit.*;
+import java.util.List;
+import com.linkedin.urls.Url;
+public class UnitTestHtmlParser {
+	private HtmlParser hp1;
+	private String htmlsegment1="hello this is a url Linkedin.com";
+	@Test
+	public void testUrlParser() {
+		List<Url> u1=hp1.parserUrl(htmlsegment1);
+		 for(Url url : u1) {
+		        System.out.println("Scheme: " + url.getScheme());
+		        System.out.println("Host: " + url.getHost());
+		        System.out.println("Path: " + url.getPath());
+		    }
+	}
+}
