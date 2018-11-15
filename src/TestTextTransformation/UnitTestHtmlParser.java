68a7e196f149be5d98c4c0acf109509daab840be
haih2@rpi.edu <haih2@rpi.edu>
2018-11-15 13:06:29 -0500

    add unit test template

--- src/TestTextTransformation/UnitTestHtmlParser.java
+++ src/TestTextTransformation/UnitTestHtmlParser.java
@@ -1,12 +1,59 @@
 package TestTextTransformation;
 import TextTransformation.*;
+
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONObject;
 import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import com.linkedin.urls.*;
+import java.util.Set;
+import java.util.Stack;
+
+import com.linkedin.urls.Url;
+import com.linkedin.urls.detection.UrlDetector;
+import com.linkedin.urls.detection.UrlDetectorOptions;
 public class UnitTestHtmlParser {
-	private HtmlParser hp1;
+	private HtmlParser parser1;
 	private String htmlsegment1="hello this is a url Linkedin.com";
+	public void testConstructor() {
+		//Implement
+	}
+	@Test
+	// TODO: Implement
+	public void testParse(String text) {
+		
+	}
+	@Test
+	// TODO: Implement
+	public void testCreateNgrams() {
+		
+	}
+	@Test
+	// TODO:Implement 
+	public void testRemoveTagAndBody() {
+		
+	}
+	@Test
+	// TODO:Implement
+	public void testCleanupTags() {
+		
+	}
+	@Test
+	//TODO: Implement
+	public void testGetTagName() {
+	}
+	
+	@Test
+	// TODO:Implement
+	public void testOutputDataStructure(){
+		
+	}
 	@Test
 	public void testUrlParser() {
+		//Implement
 	}
 }
