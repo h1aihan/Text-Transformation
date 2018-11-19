3ee4e15e801d95feeabe9eff59c0c6e0ee0a3fdb
haih2@rpi.edu <haih2@rpi.edu>
2018-11-19 11:20:44 -0500

    add unit test

--- src/TestTextTransformation/UnitTestHtmlParser.java
+++ src/TestTextTransformation/UnitTestHtmlParser.java
@@ -2,8 +2,15 @@ package TestTextTransformation;
 import TextTransformation.*;
 
 import org.apache.commons.lang3.StringUtils;
-import org.json.JSONObject;
+import org.json.*;
 import org.junit.*;
+
+import static org.junit.Assert.fail;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,42 +25,76 @@ import com.linkedin.urls.detection.UrlDetector;
 import com.linkedin.urls.detection.UrlDetectorOptions;
 public class UnitTestHtmlParser {
 	private HtmlParser parser1;
-	private String htmlsegment1="hello this is a url Linkedin.com";
+	private JSONObject json= new JSONObject(); 
+	private String simpleUrlSegment="hello this is a url Linkedin.com";
+	private String simpleHtmlString="<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\"\r\n" + 
+			" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">\r\n" + 
+			"<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\">\r\n" + 
+			"  <head>\r\n" + 
+			"    <title>Your page title here</title>\r\n" + 
+			"  </head>\r\n" + 
+			"  <body>\r\n" + 
+			"    <h1>Your major heading here</h1>\r\n" + 
+			"    <p>\r\n" + 
+			"      This is a regular text paragraph.\r\n" + 
+			"    </p>\r\n" + 
+			"    <ul>\r\n" + 
+			"      <li>\r\n" + 
+			"        First bullet of a bullet list.\r\n" + 
+			"      </li>\r\n" + 
+			"      <li>\r\n" + 
+			"        This is the <em>second</em> bullet. \r\n" + 
+			"      </li>\r\n" + 
+			"    </ul>\r\n" + 
+			"  </body>\r\n" + 
+			"</html>";
 	public void testConstructor() {
 		//Implement
+		fail("No implementation");
+		return;
 	}
 	@Test
 	// TODO: Implement
-	public void testParse(String text) {
-		
+	public void testParse() {
+		fail("No implementation");
+		return;
 	}
 	@Test
 	// TODO: Implement
 	public void testCreateNgrams() {
-		
+		fail("No implementation");
+		return;
 	}
 	@Test
 	// TODO:Implement 
 	public void testRemoveTagAndBody() {
-		
+		fail("No implementation");
+		return;
 	}
 	@Test
 	// TODO:Implement
 	public void testCleanupTags() {
-		
+		fail("No implementation");
+		return;
 	}
 	@Test
 	//TODO: Implement
 	public void testGetTagName() {
+		fail("No implementation");
+		return;
 	}
 	
 	@Test
 	// TODO:Implement
-	public void testOutputDataStructure(){
-		
+	// fix: null pointer exception
+	public void testOutputDataStructure() throws Exception{
+		this.json.put("content",simpleHtmlString);
+		parser1.parse(json);
 	}
 	@Test
 	public void testUrlParser() {
 		//Implement
+		fail("No implementation");
+		return;
 	}
 }
