4e765adff256e0ac87578f90b716d1c3f45fb010
Eva Jiang <evajiang322@gmail.com>
2018-12-10 08:16:37 -0500

    comment out jtidy

--- src/TextTransformation/Parser.java
+++ src/TextTransformation/Parser.java
@@ -10,7 +10,7 @@ import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
 import org.json.*;
-import org.w3c.tidy.Tidy;
+//import org.w3c.tidy.Tidy;
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Whitelist;
 
